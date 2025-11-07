5e30cf8cecc976bebdcfb31cecf66182f5cfaee4
Soroush Hooshyar <soroush.hooshyar@gmail.com>
2025-11-07 01:51:04 +0100

    Initial commit

---
+++ JenkinsTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using Allure.NUnit.Attributes;
+using Allure.NUnit;
+
+namespace YourProject.Tests;
+
+[AllureNUnit]
+[AllureSuite("Calculator")]
+public class SampleTests
+{
+    [Test]
+    [Category("smoke")]
+    [AllureSubSuite("Add")]
+    [AllureDescription("Simple addition test")]
+    public void Add_Works()
+    {
+        Assert.That(4.Equals(2 + 2));
+    }
+
+    [Test]
+    [Category("smoke")]
+    [AllureSubSuite("Add")]
+    [AllureDescription("Simple minus test")]
+    public void Minus_Works()
+    {
+        Assert.That(4.Equals(6 - 2));
+    }
+
+    //[Test]
+    //[Category("regression")]
+    //[AllureSubSuite("Divide")]
+    //public void Divide_ByZero_Throws()
+    //{
+    //    Assert.Throws<System.DivideByZeroException>(() => { var _ = 1 / 0; });
+    //}
+}
